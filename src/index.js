76f366896afba5110126d2f24a832909f6b5a667
Ramya Annadurai <ramyadurai2606@gmail.com>
2026-02-11 21:28:14 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 8000;
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello from express server!");
+});
+
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`);
+});
