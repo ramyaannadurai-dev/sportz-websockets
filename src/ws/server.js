8dd5f30c5ad06dde785129b1e1225fb7fc00710c
Ramya Annadurai <ramyadurai2606@gmail.com>
2026-02-18 16:02:27 +0530

    Added Websocket connection details

--- src/ws/server.js
+++ src/ws/server.js
@@ -24,23 +24,63 @@ export function attachWebSocketServer(server) {
   //   sendJson(socket, { type: "welcome" });
   //   socket.on("error", console.error);
   // });
-  wss.on("connection", (socket) => {
-    console.log("Client connected");
+  // wss.on("connection", (socket) => {
+  //   console.log("Client connected");
+
+  //   sendJson(socket, { type: "welcome" });
+
+  //   socket.on("message", (message) => {
+  //     console.log("Received:", message.toString());
+  //   });
+
+  //   socket.on("close", () => {
+  //     console.log("Client disconnected");
+  //   });
+
+  //   socket.on("error", (err) => {
+  //     console.error("Socket error:", err);
+  //   });
+
+  // });
+  wss.on("connection", async (socket, req) => {
+    socket.isAlive = true;
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
+
+    socket.subscriptions = new Set();
 
     sendJson(socket, { type: "welcome" });
 
-    socket.on("message", (message) => {
-      console.log("Received:", message.toString());
+    socket.on("message", (data) => {
+      handleMessage(socket, data);
+    });
+
+    socket.on("error", () => {
+      socket.terminate();
     });
 
     socket.on("close", () => {
-      console.log("Client disconnected");
+      cleanupSubscriptions(socket);
     });
 
-    socket.on("error", (err) => {
-      console.error("Socket error:", err);
-    });
+    socket.on("error", console.error);
   });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) return ws.terminate();
+
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, 30000);
+
+  wss.on("close", () => clearInterval(interval));
+
+  function broadcastMatchCreated(match) {
+    broadcastToAll(wss, { type: "match_created", data: match });
+  }
 
   function broadcastMatchCreated(match) {
     broadcast(wss, { type: "match_Created", data: match });
